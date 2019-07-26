6c821879739ba6a38f2eb3c3cee73c1591c97ace
Alexander <a.efimenko72@gmail.com>
2019-07-26 15:44:45 +0500

    Fix field naming in PhotoResponse

--- app/src/main/java/dev/steelahhh/skvette/network/model/PhotoResponse.kt
+++ app/src/main/java/dev/steelahhh/skvette/network/model/PhotoResponse.kt
@@ -50,7 +50,7 @@ data class UserResponse(
 data class LinksResponse(
     @Json(name = "self") val self: String?,
     @Json(name = "html") val html: String?,
-    @Json(name = "getPhotos") val photos: String?,
+    @Json(name = "photos") val photos: String?,
     @Json(name = "likes") val likes: String?,
     @Json(name = "portfolio") val portfolio: String?,
     @Json(name = "following") val following: String?,
